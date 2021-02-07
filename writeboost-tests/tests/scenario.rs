f452cc7cdf79f07b0b99604ba8aa763351132af1
Akira Hayakawa <ruby.wktk@gmail.com>
2021-02-07 16:24:10 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- writeboost-tests/tests/scenario.rs
+++ writeboost-tests/tests/scenario.rs
@@ -1,7 +1,7 @@
 use writeboost_tests::*;
 use device_mapper_tests::*;
 
-use cmd_lib::run_cmd;
+use cmd_lib::*;
 
 #[test]
 fn badblocks() {
@@ -59,5 +59,64 @@ fn comipile_ruby() {
         let cr = CompileRuby { root: &fs.mount_point };
         cr.compile();
         cr.check();
+    }
+}
+
+#[test]
+fn dbench() {
+    use fs::xfs::*;
+
+    let mut env = env();
+    let slow = env.alloc_device(Sector::MB(1600));
+    let fast = env.alloc_device(Sector::MB(300));
+    sweep_caches(&fast);
+
+    let options = vec![
+        "-t 60 1",
+        "-S -t 60 4",
+        "-s -t 60 4",
+    ];
+
+    for option in options {
+        let table = Table {
+            backing_dev: slow.path(),
+            cache_dev: fast.path(),
+            options: Options::default().writeback_threshold(70).read_cache_threshold(31),
+        };
+        let wb = Writeboost::create(table);
+        XFS::format(&wb);
+        let fs = XFS::new(&wb);
+        let pwd = &fs.mount_point;
+        proc_env_set!(PWD = pwd);
+        {
+            run_cmd!(dbench ${option}).unwrap();
+        }
+    }
+}
+
+#[test]
+fn stress() {
+    use fs::xfs::*;
+    use scenario::compile_ruby::*;
+
+    let mut env = env();
+    let slow = env.alloc_device(Sector::MB(1600));
+    let fast = env.alloc_device(Sector::MB(300));
+    sweep_caches(&fast);
+
+    let table = Table {
+        backing_dev: slow.path(),
+        cache_dev: fast.path(),
+        options: Options::default(),
+    };
+    let wb = Writeboost::create(table);
+    XFS::format(&wb);
+    let fs = XFS::new(&wb);
+    let pwd = &fs.mount_point; 
+    // 4 workers
+    // each worker writes 256MB
+    proc_env_set!(PWD = pwd);
+    {
+        run_cmd!(stress -v --timeout 60 --hdd 4 --hdd-bytes 256M).unwrap();
     }
 }
